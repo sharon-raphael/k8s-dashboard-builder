fc881cd108b7a273711ce1c0366d266b54b37d17
sharon-raphael <sharon.rc.official@gmail.com>
2025-07-09 23:40:54 +0530

    Added default config on the post endpoint

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,7 +1,6 @@
 package store
 
 import (
-	"errors"
 	"sync"
 )
 
@@ -11,15 +10,35 @@ type ConfigStore struct {
 }
 
 func NewConfigStore() *ConfigStore {
-	return &ConfigStore{}
+	defaultConfig := `config:
+dashboards:
+- name: "Sample Dashboard"
+  header: "Sample Dashboard configuration."
+  panel
+  - type: "table"
+    title: "Deployment versions"
+    data_source: "kubernetes"
+    api: pods
+    query: ""
+    namespace: ""
+    columns:
+    - header: "Name"
+      field: "metadata.name"
+    - header: "Namespace"
+      field: "metadata.namespace"
+    - header: "Status"
+      field: "status.phase"
+    - header: "image"
+      field: "spec.containers[0].image"`
+
+	return &ConfigStore{
+		config: defaultConfig,
+	}
 }
 
 func (s *ConfigStore) Get() (string, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	if s.config == "" {
-		return "", errors.New("no config found")
-	}
 	return s.config, nil
 }
 
